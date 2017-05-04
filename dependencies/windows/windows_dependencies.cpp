db9751d329371a16fe8860ed94a7d3e43b892f7b
gariksh97 <harry-123@yandex.ru>
2017-05-04 07:48:58 -0700

    Windows password

--- dependencies/windows/windows_dependencies.cpp
+++ dependencies/windows/windows_dependencies.cpp
@@ -4,12 +4,23 @@
 
 #include "windows_dependencies.h"
 #include <windows.h>
+#include <iostream>
+#include <thread>
+
 
 void notify(std::string name, std::string text) {
+    //NEED TO CREATE NEW THREAD
     std::string value = "bin\\windows\\notify-send.exe \"" + name + "\" \"" + text + "\"";
     system(value.c_str());
 };
 
 std::string inputPassword() {
-    return "null";
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode = 0;
+    GetConsoleMode(hStdin, &mode);
+    SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
+
+    std::string s;
+    getline(std::cin, s);
+    return s;
 };
