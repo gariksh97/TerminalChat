c5e9c0662dae5baf5c8408f208ebd8e3986feb64
gariksh97 <harry-123@yandex.ru>
2017-05-11 12:17:25 +0300

    With build

---
+++ cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/garik/CLionProjects/TerminalChat/dependencies/Dependencies.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/dependencies/Dependencies.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/dependencies/linux/LinuxDependencies.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/dependencies/linux/LinuxDependencies.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/dependencies/linux/tui/Chat.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/Chat.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/dependencies/linux/tui/ChatList.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/ChatList.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/dependencies/linux/tui/ChatTUI.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/ChatTUI.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/dependencies/linux/tui/TUIHelper.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/TUIHelper.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/dependencies/linux/tui/TextBox.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/TextBox.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/main.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/main.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/models/ChatModel.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/models/ChatModel.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/models/MessageModel.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/models/MessageModel.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/network/Listener.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/network/Listener.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/network/Networking.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/network/Networking.cpp.o"
+  "/home/garik/CLionProjects/TerminalChat/network/ThreadExecutor.cpp" "/home/garik/CLionProjects/TerminalChat/cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/network/ThreadExecutor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/gtk-2.0"
+  "/usr/lib/x86_64-linux-gnu/gtk-2.0/include"
+  "/usr/include/gio-unix-2.0"
+  "/usr/include/cairo"
+  "/usr/include/pango-1.0"
+  "/usr/include/atk-1.0"
+  "/usr/include/pixman-1"
+  "/usr/include/libpng12"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/harfbuzz"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "/usr/include/freetype2"
+  "/usr/include/gtk-3.0"
+  "/usr/include/at-spi2-atk/2.0"
+  "/usr/include/at-spi-2.0"
+  "/usr/include/dbus-1.0"
+  "/usr/lib/x86_64-linux-gnu/dbus-1.0/include"
+  "/usr/include/mirclient"
+  "/usr/include/mircommon"
+  "/usr/include/mircookie"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TerminalChatLinux.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TerminalChatLinux.dir/main.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/network/Networking.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/network/Listener.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/network/ThreadExecutor.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/dependencies/Dependencies.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/dependencies/linux/LinuxDependencies.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/ChatTUI.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/ChatList.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/Chat.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/TUIHelper.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/dependencies/linux/tui/TextBox.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/models/ChatModel.cpp.o"
+  "CMakeFiles/TerminalChatLinux.dir/models/MessageModel.cpp.o"
+  "TerminalChatLinux.pdb"
+  "TerminalChatLinux"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TerminalChatLinux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
